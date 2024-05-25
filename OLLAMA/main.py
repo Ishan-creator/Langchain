23c5282cc5526d88b92222d05c7c3c04eeb26c26
Ishan Pokhrel <ishanpokhrel11@gmail.com>
2024-05-25 12:21:13 +0545

    Update:RAG

--- OLLAMA/main.py
+++ OLLAMA/main.py
@@ -8,8 +8,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ["LANGCHAIN_TRACING_V2"]= 'true'
-os.environ["]LANGCHAIN_API_KEY"]= os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGCHAIN_PROJECT")
+
 
 
 prompt = ChatPromptTemplate.from_messages(
